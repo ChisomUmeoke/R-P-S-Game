4ee79292e0746888fa5086613174e90fe027f5ef
Chisom Umeoke <chisom.umeoke@gmail.com>
2021-06-01 08:38:17 -0400

    Rock, Paper,Scissors Game. First commit

---
+++ ap.js
@@ -0,0 +1,106 @@
+    const playRps = () => {
+        const cDisplayer = document.getElementById('cpdisplayer');
+        const pDisplayer = document.getElementById('pdisplayer');
+        const alert = document.getElementById('status');
+        const compSelection = document.getElementById('compSelection');
+        const pdisplay = document.querySelector('.pdisplay');
+
+        //the selection tools
+        const values = ['rock', 'paper', 'scissors'];
+        const selections = [
+            '<img src="images/rock.png" alt="rock" width=15%>',
+            '<img src="images/paper.png" alt="paper" width=15%>',
+            '<img src="images/scissors.png" alt="scissor" width=15%>'
+        ];
+
+        const playAgainBtn = document.getElementById('play-again');
+        playAgainBtn.style.display = 'none'; //button can only appear when first selection has been made
+
+        const computerPlay = () => {
+            const round = Math.floor(Math.random() * 3);
+            compSelection.innerHTML = selections[round];
+            return round;
+        }
+
+        const startComputer = () => setInterval(() => {
+            computerPlay();
+        }, 100);
+
+        let interval = startComputer();//setting intervals to computer display
+
+// conditions fpr showing winner
+        const showWinner = (computer, player) => {
+            if (computer === player) {
+                alert.innerHTML = "Tie";
+                alert.style.color = 'Black';
+            }
+
+            else if (player === 'rock') {
+                if (computer === 'scissors') {
+                    pDisplayer.innerHTML = Number(pDisplayer.innerHTML) + 1;//Number is used to convert pDiplayer/cDisplayer to an integer
+                    alert.innerHTML = "You Win";
+                    alert.style.color = 'green';
+                }
+
+                else {
+                    cDisplayer.innerHTML = Number(cDisplayer.innerHTML) + 1;
+                    alert.innerHTML = "Computer Win";
+                    alert.style.color = 'red';
+                }
+
+            }
+
+            else if (player === 'rock') {
+                if (computer === 'paper') {
+                    cDisplayer.innerHTML = Number(cDisplayer.innerHTML) + 1;
+                    alert.innerHTML = "Computer Win";
+                    alert.style.color = 'red';
+                }
+
+                else {
+                    pDisplayer.innerHTML = Number(pDisplayer.innerHTML) + 1;
+                    alert.innerHTML = "You Win";
+                    alert.style.color = 'green';
+                }
+
+            }
+
+            else if (player === 'paper') {
+                if (computer === 'scissors') {
+                    cDisplayer.innerHTML = Number(cDisplayer.innerHTML) + 1;
+                    alert.innerHTML = "Computer Win";
+                    alert.style.color = 'red';
+                }
+
+                else {
+                    pDisplayer.innerHTML = Number(pDisplayer.innerHTML) + 1;
+                    alert.innerHTML = "You Win";
+                    alert.style.color = 'green';
+                }
+            }
+        }
+
+        const playAgain = () => {
+            interval = startComputer();
+            playAgainBtn.style.display = 'none';
+        }
+        playAgainBtn.addEventListener('click', () => {
+            playAgain();
+        });
+        //loop for adding event listener to players selection
+        const selectorsElement = document.querySelectorAll('.p-selection');
+        for (let i = 0; i < 3; i++) {
+            selectorsElement[i].addEventListener('click', () => {
+                const playerValue = selectorsElement[i].dataset.value;//this picks the data-value selection from the HTML
+                pdisplay.innerHTML = selections[playerValue];//displays player selection
+                clearInterval(interval);//stops the interval count to display computer selection
+                const computerRand = computerPlay();
+                showWinner(values[computerRand], values[playerValue]);//show winer
+                playAgainBtn.style.display = 'block';
+            });
+        }
+
+    }
+    playRps()
+
+
